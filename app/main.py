354d93294e38b94e01cb22fd1e75397da0794843
Niina <170012548+niinakos@users.noreply.github.com>
2026-03-24 14:17:32 +0200

    Create main function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
 # This file will start the application
+
+from data.json_repository import JsonRepository
+from services.data_service import DataService
+from controllers.data_controller import DataController
+from gui.main_window import MainWindow
+
+def main():
+    repository = JsonRepository("database/json_database.json")
+    service = DataService(repository)
+    controller = DataController(service)
+    main_window = MainWindow(controller)
+
+    main_window.run()
+
+if __name__ == "__main__":
+    main()
